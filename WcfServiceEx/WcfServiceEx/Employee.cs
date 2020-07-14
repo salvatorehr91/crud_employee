1d41f98c6b2d7fbad3ee695bea9a9782a64a4489
salvatorehr91 <66634350+salvatorehr91@users.noreply.github.com>
2020-07-14 11:49:10 -0500

    Update Employee.cs
    Se agregó un comentario a la variable: status

--- WcfServiceEx/WcfServiceEx/Employee.cs
+++ WcfServiceEx/WcfServiceEx/Employee.cs
@@ -2,10 +2,13 @@
 {
     public class Employee
     {
+        /*This is a comment*/
         public string nombre { get; set; }
         public string apaterno { get; set; }
         public string amaterno { get; set; }
         public string fechaN { get; set; }
+        
+        // Valida si el empleado está activo o dado de baja
         public bool status { get; set; }
         public string areaemp { get; set; }
 
